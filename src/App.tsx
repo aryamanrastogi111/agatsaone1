808508a8c12bf6876ffd3dd253bce7aca0f865e6
aryamanrastogi111 <121808102+aryamanrastogi111@users.noreply.github.com>
2026-03-05 12:55:07 +0530

    Add admin panel routes

--- src/App.tsx
+++ src/App.tsx
@@ -21,17 +21,28 @@ import ShippingPolicy from "./pages/ShippingPolicy";
 import ReturnPolicy from "./pages/ReturnPolicy";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
- 
- // SDK Portal Pages
- import SDKLanding from "./pages/sdk/SDKLanding";
- import SDKAuth from "./pages/sdk/SDKAuth";
- import SDKDashboard from "./pages/sdk/SDKDashboard";
- import SDKDevices from "./pages/sdk/SDKDevices";
- import SDKDownloads from "./pages/sdk/SDKDownloads";
- import SDKCredits from "./pages/sdk/SDKCredits";
- import SDKDocs from "./pages/sdk/SDKDocs";
- import SDKSupport from "./pages/sdk/SDKSupport";
- import SDKAdmin from "./pages/sdk/SDKAdmin";
+
+// SDK Portal Pages
+import SDKLanding from "./pages/sdk/SDKLanding";
+import SDKAuth from "./pages/sdk/SDKAuth";
+import SDKDashboard from "./pages/sdk/SDKDashboard";
+import SDKDevices from "./pages/sdk/SDKDevices";
+import SDKDownloads from "./pages/sdk/SDKDownloads";
+import SDKCredits from "./pages/sdk/SDKCredits";
+import SDKDocs from "./pages/sdk/SDKDocs";
+import SDKSupport from "./pages/sdk/SDKSupport";
+import SDKAdmin from "./pages/sdk/SDKAdmin";
+
+// Admin Panel
+import AdminLayout from "./components/admin/AdminLayout";
+import AdminDashboard from "./pages/admin/Dashboard";
+import ProductsList from "./pages/admin/products/ProductsList";
+import ProductForm from "./pages/admin/products/ProductForm";
+import OrdersList from "./pages/admin/orders/OrdersList";
+import OrderDetail from "./pages/admin/orders/OrderDetail";
+import AdminCustomers from "./pages/admin/Customers";
+import AdminCoupons from "./pages/admin/Coupons";
+import AdminReviews from "./pages/admin/Reviews";
 
 const queryClient = new QueryClient();
 
@@ -59,16 +70,28 @@ const App = () => (
           <Route path="/return-policy" element={<ReturnPolicy />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-service" element={<TermsOfService />} />
-           {/* SDK Portal Routes */}
-           <Route path="/sdk" element={<SDKLanding />} />
-           <Route path="/sdk/auth" element={<SDKAuth />} />
-           <Route path="/sdk/dashboard" element={<SDKDashboard />} />
-           <Route path="/sdk/devices" element={<SDKDevices />} />
-           <Route path="/sdk/downloads" element={<SDKDownloads />} />
-           <Route path="/sdk/credits" element={<SDKCredits />} />
-           <Route path="/sdk/docs" element={<SDKDocs />} />
-           <Route path="/sdk/support" element={<SDKSupport />} />
-           <Route path="/sdk/admin" element={<SDKAdmin />} />
+          {/* SDK Portal Routes */}
+          <Route path="/sdk" element={<SDKLanding />} />
+          <Route path="/sdk/auth" element={<SDKAuth />} />
+          <Route path="/sdk/dashboard" element={<SDKDashboard />} />
+          <Route path="/sdk/devices" element={<SDKDevices />} />
+          <Route path="/sdk/downloads" element={<SDKDownloads />} />
+          <Route path="/sdk/credits" element={<SDKCredits />} />
+          <Route path="/sdk/docs" element={<SDKDocs />} />
+          <Route path="/sdk/support" element={<SDKSupport />} />
+          <Route path="/sdk/admin" element={<SDKAdmin />} />
+          {/* Admin Panel Routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="products" element={<ProductsList />} />
+            <Route path="products/new" element={<ProductForm />} />
+            <Route path="products/:id/edit" element={<ProductForm />} />
+            <Route path="orders" element={<OrdersList />} />
+            <Route path="orders/:id" element={<OrderDetail />} />
+            <Route path="customers" element={<AdminCustomers />} />
+            <Route path="coupons" element={<AdminCoupons />} />
+            <Route path="reviews" element={<AdminReviews />} />
+          </Route>
           {/* Redirects for old routes */}
           <Route path="/solutions" element={<Navigate to="/support#solutions" replace />} />
           <Route path="/trust" element={<Navigate to="/about" replace />} />
